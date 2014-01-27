d468dc4a25fded1d385f0f165205d73093498574
Oswoll <nicholas.galasinao@gmail.com>
2014-01-26 23:03:03 -0500

    Update manageDB.py
    fixed syntax

--- manageDB.py
+++ manageDB.py
@@ -4,22 +4,27 @@ import sqlite3
 
 def insertTeacher(name, room):
     conn = sqlite3.connect("ParentTeacher.db")
-    conn.execute('''INSERT INTO TEACHERS 
+    q = '''INSERT INTO TEACHERS 
     (NAME, ROOM) \
-    VALUES (?, ?)''', (name, room));
+    VALUES (?, ?)'''
+    conn.execute(q, [name, room])
+    conn.commit()
+    conn.close()
 
 def insertParent(name, sid):
     conn = sqlite3.connect("ParentTeacher.db")
-    conn.execute('''INSERT INTO PARENTS 
+    q = '''INSERT INTO PARENTS 
     (NAME, ID) \
-    VALUES (?, ?)''', (name, sid));
+    VALUES (?, ?)'''
+    conn.execute(q, [name, sid])
     conn.commit()
     conn.close()
 
 def getTeacherSchedule(name):
     conn = sqlite3.connect("ParentTeacher.db")
-    cursor = conn.execute('''SELECT TIME, PARENT FROM TEACHER 
-    WHERE NAME = ?''', (name));
+    q = '''SELECT TIME, PARENT FROM TEACHER 
+    WHERE NAME = ?'''
+    cursor = conn.execute(q, [name])
     time = cursor[0]
     parent = cursor[1]
     conn.commit()
@@ -27,8 +32,9 @@ def getTeacherSchedule(name):
 
 def getParentSchedule(name):
     conn = sqlite3.connect("ParentTeacher.db")
-    cursor = conn.execute('''SELECT TIME, TEACHER FROM PARENT
-    WHERE NAME = ?''', (name));
+    q = '''SELECT TIME, TEACHER FROM PARENT
+    WHERE NAME = ?'''
+    cursor = conn.execute(q, [name])
     time = cursor[0]
     teacher = cursor[1]
     conn.commit()
@@ -36,21 +42,25 @@ def getParentSchedule(name):
 
 def scheduleUpdate(parentName, teacherName, time):
     conn = sqlite3.connect("ParentTeacher.db")
-    cursorT = conn.execute('''SELECT TIME, PARENT FROM TEACHER 
-    WHERE NAME = ?''', (name));
-    cursorP = conn.execute('''SELECT TIME, TEACHER FROM PARENT
-    WHERE NAME = ?''', (name));
+    t = '''SELECT TIME, PARENT FROM TEACHER 
+    WHERE NAME = ?'''
+    cursorT = conn.execute(t, [name])
+    p = '''SELECT TIME, TEACHER FROM PARENT
+    WHERE NAME = ?'''
+    cursorP = conn.execute(p, [name])
     timeT = cursorT[0].append(time)
     parent = cursorT[1].append(parentName)
     timeP = cursorP[0].append(time)
     teacher = cursorP[1].append(teacherName)
-
-    conn.execute('''UPDATE PARENT
+    
+    p = '''UPDATE PARENT
     set TIME = ?, TEACHER = ?
-    WHERE NAME = ?''', (timeT, teacher, parentName));
-    conn.execute('''UPDATE TEACHER
+    WHERE NAME = ?'''
+    conn.execute(p [timet, teacher, parentName])
+    t = '''UPDATE TEACHER
     set TIME = ?, PARENT = ?
-    WHERE NAME = ?''', (timeP, parent, teacherName));
+    WHERE NAME = ?'''
+    conn.execute(t, [timeP, parent, teacherName])
     conn.commit()
     conn.close()
     
